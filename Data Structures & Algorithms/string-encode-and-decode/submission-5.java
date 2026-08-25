0f3526fda4730cd8900070f1730633228d0a467f
Noel Antony Lobo <134614847+Noel2105@users.noreply.github.com>
2026-08-25 21:58:06 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encoded = new StringBuilder("");
+        for (String s : strs) {
+            encoded.append(s.length()).append(",");
+        }
+        encoded.append(".");
+        for (String str : strs) {
+            encoded.append(str).append("#");
+        }
+        return encoded.toString();
+    }
+
+    public List<String> decode(String str) {
+        if (str.equals(".")) {
+            return new ArrayList<>();
+        }
+        int stIdx = str.indexOf(".") + 1;
+        List<String> result = new ArrayList<>();
+        String[] lengths = str.substring(0, stIdx - 1).split(",");
+        for (String len : lengths) {
+            int l = stIdx + Integer.parseInt(len);
+            result.add(str.substring(stIdx, l));
+            stIdx = l + 1;
+        }
+        return result;
+    }
+}
