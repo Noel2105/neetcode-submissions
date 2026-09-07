3fe3bfeaf79b2da213ab22235a0e4a5b93d181f5
Noel Antony Lobo <134614847+Noel2105@users.noreply.github.com>
2026-09-07 10:15:48 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] ch = s.toLowerCase().toCharArray();
+        StringBuilder str = new StringBuilder();
+        for (int i = 0 ; i < ch.length ; i++) {
+            char c =ch[i];
+            if ((c >= '0' && c <= '9') ||
+            (c >= 'A' && c <= 'Z') ||
+            (c >= 'a' && c <= 'z')) {
+                str.append(c);
+            }
+        }
+        String og = str.toString();
+        return str.reverse().toString().equals(og);
+    }
+}
